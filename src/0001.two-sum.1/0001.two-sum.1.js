2a8a67a5e9a206062a47b9db25cc86310edbc739
Yun Feng <iostream3100@gmail.com>
2021-05-07 16:32:39 +0800

    Solved Problem 1

--- src/0001.two-sum.1/0001.two-sum.1.js
+++ src/0001.two-sum.1/0001.two-sum.1.js
@@ -3,6 +3,13 @@
  * @param {number} target
  * @return {number[]}
  */
- var twoSum = function(nums, target) {
-    
+var twoSum = function(nums, target) {
+  for (let i = 0; i < nums.length; i++) {
+    let rest = target - nums[i];
+    for (let j = 0; j<nums.length; j++) {
+      if (i != j && rest == nums[j] ) {
+        return [i,j];
+      }
+    }
+  }
 };

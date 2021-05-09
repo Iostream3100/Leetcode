4aae93eb9356b034493b595a4e85274df8ceba66
Yun Feng <iostream3100@gmail.com>
2021-05-09 13:25:16 +0800

    Solved problem 2

--- src/0002.add-two-numbers.1/0002.add-two-numbers.1.1.js
+++ src/0002.add-two-numbers.1/0002.add-two-numbers.1.1.js
@@ -10,6 +10,23 @@
  * @param {ListNode} l2
  * @return {ListNode}
  */
- var addTwoNumbers = function(l1, l2) {
-    //testeststewt
+var addTwoNumbers = function(l1, l2) {
+  let carry = arguments[2];
+  let node = null;
+
+  if(l1 || l2){
+    let val1 = l1 ? l1.val : 0;
+    let val2 = l2 ? l2.val : 0;
+    let val = carry ? val1 + val2 + 1 : val1 + val2;
+    node = new ListNode(val % 10);
+
+    next1 = l1 ? l1.next : null;
+    next2 = l2 ? l2.next : null;
+    node.next = addTwoNumbers(next1, next2, val >= 10);
+  } else if(carry){
+    node = new ListNode(1);
+  }
+
+  return node;
 };
+

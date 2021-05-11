f415c59f303bac70d234a89acbe92db9a564e3dd
Yun Feng <iostream3100@gmail.com>
2021-05-11 09:08:42 +0800

    Solved problem 5 with another solution

--- src/0005.longest-palindromic-substring.5/0005.longest-palindromic-substring.5.2.js
+++ src/0005.longest-palindromic-substring.5/0005.longest-palindromic-substring.5.2.js
@@ -2,17 +2,36 @@
  * @param {string} s
  * @return {string}
  */
- var longestPalindrome = function(s) {
-
+var longestPalindrome = function(s) {
   let longestSubstirng = s[0];
-  for(let len = 2; len <= s.length; len++) { //length of tue substring
-    for(let i = 0; i <= s.length - len; i++) { //starting point
-      if(s[i] == s[i + len - 1] && (len == 2 || isPalindrome[i + 1][i + len - 2])) {
-        isPalindrome[i][i + len - 1] = true;
-        longestSubstirng = s.substr(i, len);
+  for(let i = 0; i < s.length; i++) { //mid point
+    for(let j = 2; j <= Math.min(2 * (i + 1), 2 * (s.length - i)); j += 2) { //the length of the substring is odd
+      let leftPos = Math.floor(i - j / 2 + 1);
+      let rightPos = Math.floor(i + j/2);
+      if(s[leftPos] == s[rightPos]) {
+        if(longestSubstirng.length < j) {
+          longestSubstirng = s.substr(leftPos, j);
+        }
+      } else {
+        break;
+      }
+    }
+
+    for(let j = 3; j <= Math.min(2 * (i + 1), 2 * (s.length - i)); j += 2) { //the length of the substring is odd
+      let leftPos = Math.floor(i - j / 2 + 1);
+      let rightPos = Math.floor(i + j/2);
+      if(s[leftPos] == s[rightPos]) {
+        if(longestSubstirng.length < j) {
+          longestSubstirng = s.substr(leftPos, j);
+        }
+      } else {
+        break;
       }
     }
   }
 
   return longestSubstirng;
 };
+
+console.log(longestPalindrome('aaaaa'));
+

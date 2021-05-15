0180b15c147babddb30b07c6838c8fdf88448480
Yun Feng <iostream3100@gmail.com>
2021-05-15 23:04:36 +0800

    Modified problem 15 solution 2

--- src/0015.3Sum.15/0015.3Sum.15.2.js
+++ src/0015.3Sum.15/0015.3Sum.15.2.js
@@ -3,17 +3,22 @@
  * @return {number[][]}
  */
 var threeSum = function(nums) {
-  nums.sort((a, b) => a - b);
   let triplets = [];
+  if(nums.length < 3) {
+    return triplets;
+  }
 
-  for(let i = 0; i < nums.length; i++) {
+  nums.sort((a, b) => a - b);
+
+  for(let i = 0; i < nums.length - 2; i++) {
+    if(nums[i] > 0){
+      return triplets;
+    }
     if(i > 0 && nums[i] == nums[i - 1]) {
       continue;
     }
 
-    let l = i + 1;
-    let r = nums.length - 1;
-    while(l < r) {
+    for(let l = i + 1, r = nums.length - 1; l < r; ) {
       let sum = nums[i] + nums[l] + nums[r];
       if(sum < 0) {
         l++;
@@ -23,8 +28,10 @@ var threeSum = function(nums) {
         triplets.push([nums[i], nums[l], nums[r]]);
         do {
           l++;
-        } while(l < nums.length - 1 && nums[l] == nums[l - 1])
-        r--;
+        } while(l < r && nums[l] == nums[l - 1])
+        do {
+          r--;
+        } while(l < r && nums[r] == nums[r + 1])
       }
     }
   }
@@ -32,7 +39,7 @@ var threeSum = function(nums) {
   return triplets;
 };
 
-console.log(threeSum([-2,0,0,2,2]));
+// console.log(threeSum([-2,0,0,2,2]));
 
 // console.log(threeSum([-4,-2,-2,-2,0,1,2,2,2,3,3,4,4,6,6]));
 // console.log(threeSum([-1,0,1,2,-1,-4]));

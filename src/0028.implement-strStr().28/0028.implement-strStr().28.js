c88993102a31ddabfa2d89927528b87241b75579
Yun Feng <iostream3100@gmail.com>
2021-06-23 15:46:31 +0800

    Solved problem 29

--- src/0028.implement-strStr().28/0028.implement-strStr().28.js
+++ src/0028.implement-strStr().28/0028.implement-strStr().28.js
@@ -4,5 +4,9 @@
  * @return {number}
  */
 var strStr = function(haystack, needle) {
-  
+  let next = [-1];
+  for(let i = 1; i < needle.length; ++i) {
+    let pos = i;
+    while(pos != -1 && needle[next[pos]] != needle[i])
+  }
 };

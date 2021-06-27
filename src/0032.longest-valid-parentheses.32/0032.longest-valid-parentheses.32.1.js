d7d21a69aff5612d16bb1d30271ebace1ec28cec
Yun Feng <iostream3100@gmail.com>
2021-06-27 21:47:26 +0800

    Solved problem 32

--- src/0032.longest-valid-parentheses.32/0032.longest-valid-parentheses.32.1.js
+++ src/0032.longest-valid-parentheses.32/0032.longest-valid-parentheses.32.1.js
@@ -3,5 +3,16 @@
  * @return {number}
  */
 var longestValidParentheses = function(s) {
-    
+  let maxLen = 0;
+  for(let i = 0; i < s.length - 1; ++i) {
+    let cnt = 0;
+    for(let j = i; j < s.length; ++j) {
+      if(s[j] == '(') cnt++;
+      else cnt --;
+      if(cnt < 0) break;
+      if(cnt == 0 && maxLen < j - i + 1) maxLen = j - i + 1;
+    }
+  }
+  
+  return maxLen;
 };
---
+++ src/0032.longest-valid-parentheses.32/0032.longest-valid-parentheses.32.2.js
@@ -0,0 +1,116 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var longestValidParentheses = function(s) {
+  if(s.length <= 1) return 0;
+
+  let l = 0;
+  while(l < s.length - 1 && s[l]!='(') l++;//find the first left parenthesis
+  if(l == s.length - 1) return 0;
+  let r = l + 1;
+
+  let cnt = 1;
+  let maxLen = 0;
+
+  while(r < s.length) {
+    cnt = (s[r] == '(') ? cnt + 1 : cnt - 1;
+
+    // console.log(l,r,cnt, maxLen);
+    if(cnt < 0) { //reset two pointers
+      l = r + 1;
+      while(s[l] != '(' && l < s.length - 1) l++;
+      r = l + 1;
+      cnt = (s[l] == '(') ? 1 : -1;
+    } else {
+      if(cnt == 0) { //update max length
+        maxLen = Math.max(maxLen, (r - l + 1));
+      }
+      r++;
+    } 
+  }
+
+  
+  if(cnt > 0) {
+    r = s.length - 1;
+    while(r > l && s[r] != ')') r--; //find the first ')' from the end
+    let rCnt = 0;
+    for(let i = r; i >= l; --i) {
+      rCnt = (s[i] == ')') ? rCnt + 1 : rCnt - 1;
+      if(rCnt < 0) break;
+      if(rCnt == 0) {
+        maxLen = Math.max(maxLen, (r - i + 1));
+      }
+    }
+  }
+
+  s = s.split("").reverse().join("");
+  let li = [];
+  for(let i = 0; i < s.length; i++) {
+    li.push(s[i] = s[i] == '(' ? ')' : '(')
+  }
+  s = li.join("");
+
+  l = 0;
+  while(l < s.length - 1 && s[l]!='(') l++;//find the first left parenthesis
+  r = l + 1;
+
+  cnt = 1;
+
+  while(r < s.length) {
+    cnt = (s[r] == '(') ? cnt + 1 : cnt - 1;
+
+    // console.log(l,r,cnt, maxLen);
+    if(cnt < 0) { //reset two pointers
+      l = r + 1;
+      while(s[l] != '(' && l < s.length - 1) l++;
+      r = l + 1;
+      cnt = (s[l] == '(') ? 1 : -1;
+    } else {
+      if(cnt == 0) { //update max length
+        maxLen = Math.max(maxLen, (r - l + 1));
+      }
+      r++;
+    } 
+  }
+
+  if(cnt > 0) {
+    r = s.length - 1;
+    while(r > l && s[r] != ')') r--; //find the first ')' from the end
+    let rCnt = 0;
+    for(let i = r; i >= l; --i) {
+      rCnt = (s[i] == ')') ? rCnt + 1 : rCnt - 1;
+      if(rCnt < 0) break;
+      if(rCnt == 0) {
+        maxLen = Math.max(maxLen, (r - i + 1));
+      }
+    }
+  }
+  
+  return maxLen;
+};
+
+console.log(longestValidParentheses("()"));
+// console.log(longestValidParentheses(""));
+// console.log(longestValidParentheses("(()"));
+// console.log(longestValidParentheses("(())"));
+// console.log(longestValidParentheses("()))"));
+// console.log(longestValidParentheses("())((())))"));
+// console.log(longestValidParentheses(")()())"));
+// // console.log(longestValidParentheses("()(()"));
+// console.log(longestValidParentheses("(())("));
+// console.log(longestValidParentheses("()(())("));
+// console.log(longestValidParentheses("()(()"));
+// console.log(longestValidParentheses("((((())()"));
+// console.log(longestValidParentheses("((()()(()((()"));
+// console.log(longestValidParentheses("((()()(()((()".split("").reverse().join("")))
+
+
+// let s ="((()";
+// s = s.split("").reverse().join("");
+// let l = [];
+// console.log(s);
+// for(let i = 0; i < s.length; i++) {
+//   l.push(s[i] = s[i] == '(' ? ')' : '(')
+// }
+// console.log(l.join(""));
